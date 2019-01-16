8f8e3596ad03bdaaf23eb220686192d55bd1f21a
superbabo <44689542+Pran0103@users.noreply.github.com>
2019-01-15 19:33:34 -0800

    Added TurnToHeading Command

---
+++ NRGRobot2019/src/main/java/frc/robot/commands/TurnToHeading.java
@@ -0,0 +1,55 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+import frc.robot.RobotMap;
+
+public class TurnToHeading extends Command {
+  private final double DEFAULT_TURN_TOLERANCE = 5.0;
+
+  private double desiredHeading;
+  private double maxPower;
+  public TurnToHeading(double desiredHeading, double maxPower) {
+    this.desiredHeading = desiredHeading;
+    this.maxPower = Math.abs(maxPower);
+    this.requires (Robot.drive);
+    }
+    
+    // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+    Robot.drive.turnToHeadingInit(this.desiredHeading, DEFAULT_TURN_TOLERANCE);
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    Robot.drive.turnToHeadingExecute();
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return Robot.drive.turnToHeadingOnTarget();
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+    Robot.drive.turnToHeadingEnd();
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+    end();
+  }
+}
--- NRGRobot2019/src/main/java/frc/robot/subsystems/Drive.java
+++ NRGRobot2019/src/main/java/frc/robot/subsystems/Drive.java
@@ -12,15 +12,21 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import frc.robot.RobotMap;
 import frc.robot.commands.ManualDrive;
+import frc.robot.utilities.SimplePIDController;
 
 /**
  * Add your docs here.
  */
 public class Drive extends Subsystem {
+  private final double DEFAULT_TURN_P = 0.2;
+  private final double DEFAULT_TURN_I = 0;
+  private final double DEFAULT_TURN_D = 0;
+
   private SpeedControllerGroup leftMotor = new SpeedControllerGroup(RobotMap.driveFrontLeftMotor,RobotMap.driveBackLeftMotor);
   private SpeedControllerGroup rightMotor = new SpeedControllerGroup(RobotMap.driveFrontRightMotor, RobotMap.driveBackRightMotor);
   private DifferentialDrive motivator = new DifferentialDrive(leftMotor, rightMotor);
-  
+  private SimplePIDController turnPIDController;
+
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
@@ -35,5 +41,25 @@ public class Drive extends Subsystem {
   }
   public void stopMotor () {
     motivator.stopMotor();
+  }
+
+  public void turnToHeadingInit(double desiredHeading, double tolerance) {
+    this.turnPIDController = new SimplePIDController(DEFAULT_TURN_P, DEFAULT_TURN_I, DEFAULT_TURN_D).
+      setSetpoint(desiredHeading).
+      setAbsoluteTolerance(tolerance);
+  }
+
+  public void turnToHeadingExecute(){
+    double currentPower = this.turnPIDController.update(RobotMap.navx.getAngle());
+    this.tankDrive(currentPower, -currentPower); 
+  }
+
+  public boolean turnToHeadingOnTarget() {
+    return this.turnPIDController.onTarget();
+  }
+
+  public void turnToHeadingEnd() {
+    this.stopMotor();
+    this.turnPIDController = null;
   }
 }
