8c863f2642e9b7702b1d10185abc658da5499a8b
Nipun Chhajed <nipunchhajed@gmail.com>
2019-02-09 00:21:08 -0800

    Lidar base class done - need to test

--- NRGRobot2019/src/main/java/frc/robot/utilities/Lidar.java
+++ NRGRobot2019/src/main/java/frc/robot/utilities/Lidar.java
@@ -1,42 +1,82 @@
 package frc.robot.utilities;
 
 import java.io.IOException;
-import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
 import edu.wpi.first.wpilibj.I2C;
 
 public class Lidar {
-    protected final int address = 0x29;
+	protected final int address = 0x29;
+	private final int SYSRANGE_START = 0x00;
+	private final int SYSTEM_INTERRUPT_CLEAR = 0x0B;
+	private final int RESULT_RANGE_STATUS = 0x14;
+	private final int RESULT_INTERRUPT_STATUS = 0x13;
+	private int ioTimeout;
+	private int stopVariable = 0;
     private I2C sensor; 
 
     public Lidar(I2C.Port port) {
+		ioTimeout = 0;
         sensor = new I2C(port, this.address);
-
+		
         //LIDAR setup - to be able to access the lidar sensor
         sensor.write((byte) 0x88, (byte) 0x00);
 		sensor.write((byte) 0x80, (byte) 0x01);
 		sensor.write((byte) 0xFF, (byte) 0x01);
 		sensor.write((byte) 0x00, (byte) 0x00);
+		this.stopVariable = this.readU8(this.sensor, 0x91);
 		sensor.write((byte) 0x00, (byte) 0x01);
 		sensor.write((byte) 0xFF, (byte) 0x00);
-        sensor.write((byte) 0x80, (byte) 0x00);
+		sensor.write((byte) 0x80, (byte) 0x00);
     }
-    public static int readU8(I2C sensor, int reg) {
+
+    private int readU8(I2C sensor, int register) {
+        //this method reads an unsigned byte from the device
 		int result = 0;
         byte[] buffer = new byte[1];
-        result = sensor.read(reg, 1, buffer[0]);
+        sensor.read(register, 1, buffer);
+        result = buffer[0];
 		return result; // & 0xFF;
     }
     
-    public static int readU16BE(I2C sensor, int register) throws IOException{
+    private int readU16BE(I2C sensor, int register) throws IOException{
+        //method reads 2 bytes of BIG ENDIANS
 		return readU16(sensor, register, ByteOrder.BIG_ENDIAN);
 	}
 
-	public static int readU16(I2C sensor, int register, ByteOrder by) throws IOException {
+	private int readU16(I2C sensor, int register, ByteOrder by) throws IOException {
+        //helper method for method above
 		int hi = readU8(sensor, register);
 		int lo = readU8(sensor, register + 1);
 		return ((by ==  ByteOrder.BIG_ENDIAN) ? (hi << 8) + lo : (lo << 8) + hi); // & 0xFFFF;
+	}
+
+	public int range() throws IOException {
+		sensor.write((byte)0x80, (byte)0x01);
+		sensor.write((byte)0xFF, (byte)0x01);
+		sensor.write((byte)0x00, (byte)0x00);
+		sensor.write((byte)0x91, (byte)this.stopVariable);
+		sensor.write((byte)0x00, (byte)0x01);
+		sensor.write((byte)0xFF, (byte)0x00);
+		sensor.write((byte)0x80, (byte)0x00);
+		sensor.write((byte)SYSRANGE_START, (byte)0x01);
+
+		long start = System.currentTimeMillis();
+		while ((this.readU8(this.sensor, SYSRANGE_START) & 0x01) > 0) {
+			if (this.ioTimeout > 0 && ((System.currentTimeMillis() - start) / 1_000) >= this.ioTimeout) {
+				throw new RuntimeException("Timeout waiting for VL53L0X!");
+			}
+		}
+		start = System.currentTimeMillis();
+		while ((this.readU8(this.sensor, RESULT_INTERRUPT_STATUS) & 0x07) == 0) {
+			if (this.ioTimeout > 0 && ((System.currentTimeMillis() - start) / 1_000) >= this.ioTimeout) {
+				throw new RuntimeException("Timeout waiting for VL53L0X!");
+			}
+		}
+
+		int rangeMm = this.readU16BE(this.sensor, RESULT_RANGE_STATUS + 10);
+		sensor.write((byte)SYSTEM_INTERRUPT_CLEAR, (byte)0x01);
+		return rangeMm;
 	}
 
 }
