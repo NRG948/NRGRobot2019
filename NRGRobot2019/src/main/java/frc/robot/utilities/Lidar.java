67a6408dd213a700c365b371b1a4f931bcd6304b
Nipun Chhajed <nipunchhajed@gmail.com>
2019-02-09 20:47:20 -0800

    debug print, checking if we actually integers from theses methods.

--- NRGRobot2019/src/main/java/frc/robot/utilities/Lidar.java
+++ NRGRobot2019/src/main/java/frc/robot/utilities/Lidar.java
@@ -76,7 +76,9 @@ public class Lidar {
 		// 		throw new RuntimeException("Timeout waiting for VL53L0X!");
 		// 	}
 		// }
-		System.out.println(this.readU16BE(this.sensor, RESULT_RANGE_STATUS + 10));
+		
+		System.out.println("hi: " + readU8(this.sensor, RESULT_RANGE_STATUS) + " lo: " + readU8(this.sensor, RESULT_RANGE_STATUS + 1));
+		//System.out.println(this.readU16BE(this.sensor, RESULT_RANGE_STATUS + 10));
 		int rangeMm = this.readU16BE(this.sensor, RESULT_RANGE_STATUS + 10);
 		sensor.write((byte)SYSTEM_INTERRUPT_CLEAR, (byte)0x01);
 		return rangeMm;
