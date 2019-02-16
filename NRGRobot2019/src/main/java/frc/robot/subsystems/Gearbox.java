a8a3996977bda9dfa062c60b317016f7af214978
SourishMurthy1 <murthy.sourish@outlook.com>
2019-02-16 14:00:56 -0800

    Cleaned up more code

--- NRGRobot2019/src/main/java/frc/robot/subsystems/Gearbox.java
+++ NRGRobot2019/src/main/java/frc/robot/subsystems/Gearbox.java
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
@@ -13,11 +6,9 @@ import frc.robot.RobotMap;
 import frc.robot.commands.GearShift;
 
 /**
- * Add your docs here.
+ * A Subsystem to control gearbox shifting.
  */
 public class Gearbox extends Subsystem {
-  // Put methods for controlling this subsystem
-  // here. Call these from Commands.
 
   public enum Gear {
     HIGH, LOW;
