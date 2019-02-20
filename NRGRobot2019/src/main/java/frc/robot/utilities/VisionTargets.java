e7e7582ad4185477a146420639e4c813eafbfa6b
Ed Reed <reed.ej@hotmail.com>
2019-02-19 18:23:41 -0800

    Sort vision targets by nearness to center

--- NRGRobot2019/src/main/java/frc/robot/utilities/VisionTargets.java
+++ NRGRobot2019/src/main/java/frc/robot/utilities/VisionTargets.java
@@ -18,50 +18,30 @@ public class VisionTargets {
     private static final double HALF_IMAGE_FOV = Math.toRadians(31.1);
     private static final double HALF_IMAGE_WIDTH = 160;
     private static final double TARGET_WIDTH = 8.0;
-    public Target left;
-    public Target right;
+
+    private ArrayList<TargetPair> targetPairs = new ArrayList<TargetPair>();
 
     public void update() {
-        this.left = null;
-        this.right = null;
-        String[] targetsJson = SmartDashboard.getStringArray("Vision/targets", NO_TARGETS);
-        ArrayList<Target> newTargets = new ArrayList<Target>();
+        ArrayList<TargetPair> newTargetPairs = new ArrayList<TargetPair>();
+        String[] targetsJson = SmartDashboard.getStringArray("Vision/targetPairs", NO_TARGETS);
         GsonBuilder builder = new GsonBuilder();
         Gson gson = builder.create();
         for (int i = 0; i < targetsJson.length; i++) {
-            newTargets.add(gson.fromJson(targetsJson[i], Target.class));
+            newTargetPairs.add(gson.fromJson(targetsJson[i], TargetPair.class));
         }
-        if (newTargets.size() >= 2) {
-            for (int i = 0; i < newTargets.size() - 1; ++i) {
-                Target current = newTargets.get(i);
-                if (current.getSide() == Target.Side.LEFT) {
-                    Target nextTarget = newTargets.get(i + 1);
-                    if (nextTarget.getSide() == Target.Side.RIGHT) {
-                        this.left = current;
-                        this.right = nextTarget;
-                        break;
-                    }
-                }
-            }
-        }
+        this.targetPairs = newTargetPairs;
     }
 
     public boolean hasTargets() {
-        return this.left != null && this.right != null;
+        return !this.targetPairs.isEmpty();
+    }
+
+    private TargetPair getDesiredTargets() {
+        return this.targetPairs.get(0);
     }
 
     public Point getCenterOfTargets() {
-        Point centerPoint = new Point();
-        Point leftCenter = this.left.getCenter();
-        Point rightCenter = this.right.getCenter();
-        centerPoint.x = ((leftCenter.x + rightCenter.x) / 2);
-        centerPoint.y = ((leftCenter.y + rightCenter.y) / 2);
-        if (this.left.getIsCameraInverted()) {
-            centerPoint.x = -centerPoint.x;
-        } else {
-            centerPoint.y = -centerPoint.y;
-        }
-        return centerPoint;
+        return getDesiredTargets().getCenterOfTargets();
     }
 
     public double getAngleToTarget() {
@@ -75,7 +55,8 @@ public class VisionTargets {
     }
 
     public double getDistanceToTarget() {
-        double targetWidth = (this.right.getMinX().x - this.left.getMaxX().x);
+        TargetPair desiredTarget = getDesiredTargets();
+        double targetWidth = (desiredTarget.right.getMinX().x - desiredTarget.left.getMaxX().x);
         return TARGET_WIDTH * 2 * HALF_IMAGE_WIDTH / (2 * targetWidth * Math.tan(HALF_IMAGE_FOV));
     }
 }
