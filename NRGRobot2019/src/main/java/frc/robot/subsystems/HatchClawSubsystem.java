d9293f3b56873bd45952f64ceed5e918d1ecd27c
Sourish <murthysourish31@gmail.com>
2019-03-09 14:08:10 -0800

    added boolean box for hatch claw state for shuffleboard

--- NRGRobot2019/src/main/java/frc/robot/subsystems/HatchClawSubsystem.java
+++ NRGRobot2019/src/main/java/frc/robot/subsystems/HatchClawSubsystem.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Robot;
 import frc.robot.RobotMap;
 import frc.robot.commands.HatchClaw;
@@ -22,9 +23,11 @@ public class HatchClawSubsystem extends Subsystem {
 
   public void setClawOpen() {
     RobotMap.hatchClawSolenoid.set(Robot.isPracticeBot() ? Value.kReverse : Value.kForward);
+    SmartDashboard.putBoolean("Hatch Claw/State", true);
   }
 
   public void setClawClose() {
     RobotMap.hatchClawSolenoid.set(Robot.isPracticeBot() ? Value.kForward : Value.kReverse);
+    SmartDashboard.putBoolean("Hatch Claw/State", false);
   }
 }
