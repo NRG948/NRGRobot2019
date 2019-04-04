97a5ba044c7c0b3aec9b7ba79a7a71cb5a2c18c5
Sourish <murthysourish31@gmail.com>
2019-04-04 16:30:37 -0700

    changed camera angle skew to match suffle board

--- NRGRobot2019/src/main/java/frc/robot/utilities/NRGPreferences.java
+++ NRGRobot2019/src/main/java/frc/robot/utilities/NRGPreferences.java
@@ -55,7 +55,7 @@ public class NRGPreferences {
         
         DRIVE_TO_VISION_TAPE_MIN_POWER("VisionMinPower", 0.15),
         DRIVE_TO_VISION_TAPE_MAX_POWER("VisionMaxPower", 0.65 ),
-        CAMERA_ANGLE_SKEW("CameraAngleSkew", -2.2),
+        CAMERA_ANGLE_SKEW("CameraAngleSkew", -1.7),
         CAMERA_DISTANCE_SCALE("CameraDistanceScale", 1.0),
         CAMERA_ANGLE_SCALE("CameraAngleScale", 0.8),
 
