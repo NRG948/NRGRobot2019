4f37ca2a1e8a2949709a11401d523a7bf976b4ac
Sourish <murthysourish31@gmail.com>
2019-04-06 12:09:10 -0700

    tweak cargo first hatch close from feeder distance

--- NRGRobot2019/src/main/java/frc/robot/commandGroups/AutonomousRoutines.java
+++ NRGRobot2019/src/main/java/frc/robot/commandGroups/AutonomousRoutines.java
@@ -145,7 +145,7 @@ public class AutonomousRoutines extends CommandGroup {
       return;
 
     case CARGO_FIRST_HATCH_CLOSE:
-      addSequential(new DriveDistanceOnHeading(autoFeederPosition == AutoFeederPosition.RIGHT_FEEDER ? -190 : 190, 250,
+      addSequential(new DriveDistanceOnHeading(autoFeederPosition == AutoFeederPosition.RIGHT_FEEDER ? -190 : 190, 238,
           -1.0, 2.0));
       addSequential(new TurnToHeading(autoFeederPosition == AutoFeederPosition.RIGHT_FEEDER ? -85 : 85, turnPower));
       addSequential(new DeliverHatch());
